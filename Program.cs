c8b4358355b420c5f9980ce1a5b692639ea2fc36
1SurajDesai <surajdesai219@gmail.com>
2022-02-21 22:02:12 +0530

    Adding Program.cs program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace LineComparisonProblem
+{
+    public class Program
+    { 
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program.");
+            //CalLengthOfLine.CalculateLength();
+            //EqualityOfLines.CheckEqualityOfLines();
+            //LineComparison.LineComparison();
+
+        }
+    }
+}
